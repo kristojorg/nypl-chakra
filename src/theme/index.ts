8df6e71b1baf35ccfce62378d4c907634af2ad09
kristojorg <kristo.jorgenson@gmail.com>
2021-04-26 11:20:31 -0400

    add theme

---
+++ src/theme/index.ts
@@ -0,0 +1,11 @@
+import { extendTheme } from '@chakra-ui/react';
+
+import Button from './components/button';
+
+const theme = extendTheme({
+  components: {
+    Button,
+  },
+});
+
+export default theme;
